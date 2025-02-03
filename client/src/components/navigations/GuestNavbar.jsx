d7071ead4334792c8425f6ce1195dbad9cb3b02d
“dsansevic” <sansevicdora@gmail.com>
2025-02-03 09:11:39 +0100

    navlinks design update

--- client/src/components/navigations/GuestNavbar.jsx
+++ client/src/components/navigations/GuestNavbar.jsx
@@ -9,18 +9,18 @@ const GuestNavbar = () => {
                 <img src={fleetflowLogo} alt="FleetFlow Logo" className="h-7 w-auto " />
           </NavLink>
             <div className="flex space-x-2">
-              <NavLink 
-                  to="/signup"
-                  className="px-6 py-3 bg-white text-brand-dark font-semibold rounded-lg border border-brand-light hover:bg-gray-50"
-              >
+            <NavLink 
+                to="/signup"
+                className="w-20 sm:w-auto px-3 py-3 sm:px-6 sm:py-3 bg-white text-brand-dark font-semibold rounded-lg border border-brand-light hover:bg-gray-50 text-sm leading-none"
+            >
                 Sign up
-              </NavLink>
-              <NavLink 
-                  to="/login"
-                  className="px-6 py-3 border bg-brand-dark border-white text-white font-semibold rounded-lg hover:bg-brand-base"
-              >
-                  Sign In
-              </NavLink>
+            </NavLink>
+            <NavLink 
+                to="/login"
+                className="w-20 sm:w-auto px-3 py-3 sm:px-6 sm:py-3 border bg-brand-dark border-white text-white font-semibold rounded-lg hover:bg-brand-base text-sm leading-none"
+            >
+                Sign In
+            </NavLink>
             </div>    
         </nav>
       </div>
