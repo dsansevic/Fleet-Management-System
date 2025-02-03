15ce907f60bad2492e1939e934a7a0066c8b54a9
“dsansevic” <sansevicdora@gmail.com>
2025-02-03 02:58:34 +0100

    Replace process.env with import.meta.env

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: process.env.BACKEND_URL || "http://localhost:3000",
+    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
     withCredentials: true,
 });
 
 export const publicApiClient = axios.create({
-    baseURL: process.env.BACKEND_URL || "http://localhost:3000",
+    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
     withCredentials: false,
 });
